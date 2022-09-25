cb487f3e6ae177d3c32c4d9ef330719a544be075
Jesus <jesusrafaellh@gmail.com>
2022-09-25 12:33:50 -0400

    navbar effect scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,28 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 function Navbar() {
+	const [navScroll, setNavScroll] = useState(false);
+
+	function classNames(...clasess: any[]) {
+		const aux = clasess.filter(Boolean).join(' ');
+		console.log(aux);
+		return aux;
+	}
+
+	useEffect(() => {
+		const handleNavScroll = () => {
+			console.log('scroll change');
+			setNavScroll(window.scrollY >= 90);
+		};
+		return window.addEventListener('scroll', handleNavScroll);
+	});
+
 	return (
-		<nav className='nav'>
+		<nav className={classNames(navScroll ? 'nav-scroll' : '', 'nav')}>
+			{/*
+		<nav className={`transition ${navScroll ? 'nav nav-scroll' : 'nav'}`}>
+		*/}
 			<div className='container flex flex-wrap justify-between items-center mx-auto'>
 				<Link href='/'>
 					<span className='flex items-center cursor-pointer self-center text-xl font-semibold whitespace-nowrap text-white'>
@@ -14,14 +34,16 @@ function Navbar() {
 					type='button'
 					className='inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-black-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500'
 					aria-controls='mobile-menu-2'
-					aria-expanded='false'>
+					aria-expanded='false'
+				>
 					<span className='sr-only'>Open main menu</span>
 					<svg
 						className='w-6 h-6'
 						aria-hidden='true'
 						fill='currentColor'
 						viewBox='0 0 20 20'
-						xmlns='http://www.w3.org/2000/svg'>
+						xmlns='http://www.w3.org/2000/svg'
+					>
 						<path
 							//fill-rule='evenodd'
 							d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
@@ -31,7 +53,8 @@ function Navbar() {
 				</button>
 				<div
 					className='hidden justify-between items-center w-full md:flex md:w-auto md:order-1'
-					id='navbar-sticky'>
+					id='navbar-sticky'
+				>
 					<ul className='flex flex-col p-4 mt-4rounded-lg border border-white-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-white-800 md:dark:bg-white-900 dark:border-white-700'>
 						<li className='h6'>
 							<a href='#' className='nav-item' aria-current='page'>
