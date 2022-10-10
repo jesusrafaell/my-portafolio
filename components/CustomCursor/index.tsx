d78c0840840ff2a01d9d3d04f201e55a3d64e01b
Jesus <jesusrafaellh@gmail.com>
2022-10-09 21:58:17 -0400

    cursor

--- components/CustomCursor/index.tsx
+++ components/CustomCursor/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 //import CustomCursorContext from './context/CustomCursorContex';
 
 const CustomCursor = () => {
-	const secondaryCursor = useRef<HTMLElement | HTMLDivElement>(null);
+	const secondaryCursor = useRef<HTMLDivElement>(null);
 
 	const [showCursor, setShowCursor] = useState(true);
 
